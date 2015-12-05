c642e24021cbbf130ee34b81c7669dfae39afd9e
gopinathdanda <gopinath.danda@gmail.com>
2015-12-05 17:41:23 -0500

    Add post data

--- json/add.php
+++ json/add.php
@@ -1,6 +1,15 @@
 <?php
 if(isset($_POST['submit'])){
-	echo "if in";
+	$label = $_POST['label'].'.json';
+	$file = file_get_contents($label);
+	$data = json_decode($file);
+	unset($file);//prevent memory leaks for large json.
+	unset($_POST['label'],$_POST['submit']);
+	$_POST['amount'] = intval($_POST['amount']);
+	$data[] = $_POST;
+	//print_r($data);
+	file_put_contents($label,json_encode($data));
+	unset($data);
 }else{
 	//echo "else in";
 }
